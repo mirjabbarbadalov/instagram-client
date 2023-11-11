bec73cc6a7758b744131df6575b57091bf69b21c
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-11-11 17:09:56 +0400

    Add profile

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,15 +1,53 @@
+import { BsGrid3X3 } from "react-icons/bs";
+import { BsBookmarkHeart } from "react-icons/bs";
+BsBookmarkHeart;
 function Profile() {
   return (
-    <div>
-      <div className="w-[188px] h-[188px] rounded-[50%] bg-slate-200"></div>
-      <div>
-        <div>
-          <p>mrcbedelov</p>
-          <button>Edit profile</button>
-          <button>View profile</button>
+    <>
+      <div className="flex items-center justify-center mt-10 gap-10">
+        <div className="w-[160px] h-[160px] rounded-[50%] bg-slate-200"></div>
+        <div className="flex flex-col items-start gap-5">
+          <div className="flex ">
+            <p className="text-lg">mrcbedelov</p>
+            <button className="ml-[30px] bg-[#ebebeb] hover:bg-[#dbdbdb] font-medium py-1 px-4 rounded-lg">
+              Edit profile
+            </button>
+            <button className="ml-[10px] bg-[#ebebeb] hover:bg-[#dbdbdb] font-medium py-1 px-4 rounded-lg ">
+              View profile
+            </button>
+          </div>
+          <div className="flex gap-[56px]">
+            <p>
+              <span className="font-semibold">20</span> posts
+            </p>
+            <p>
+              <span className="font-semibold">200</span> followers
+            </p>
+            <p>
+              <span className="font-semibold">200</span> following
+            </p>
+          </div>
+          <div>
+            <p className="text-sm font-semibold">Mirjabbar Badalov</p>
+          </div>
         </div>
       </div>
-    </div>
+      <hr className="w-[800px] border-t-[1.5px] border-[#dbdbdb] mt-10 mx-auto" />
+      <div className="flex w-[800px] mt-10 mx-auto items-center justify-around">
+        <div className="flex items-center gap-2 cursor-pointer">
+          <p>
+            <BsGrid3X3 />
+          </p>
+          <p>POSTS</p>
+        </div>
+        <div className="flex items-center gap-2 cursor-pointer">
+          <p>
+            <BsBookmarkHeart />
+          </p>
+          <p>FAVORITES</p>
+        </div>
+      </div>
+    </>
   );
 }
 
