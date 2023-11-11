79a3466a62101cca913c23c0a5c13dbef4571532
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-11-11 16:21:15 +0400

    Update profile page

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <div className="w-[188px] h-[188px] rounded-[50%] bg-slate-200"></div>
+      <div>
+        <div>
+          <p>mrcbedelov</p>
+          <button>Edit profile</button>
+          <button>View profile</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
