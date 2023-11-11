c78e76139b5216cd70b01c4ecca5303a015aae37
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-11 16:13:32 +0400

    Add Router

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <NavLink to={"/"}></NavLink>
+      <NavLink to={"/feed"}></NavLink>
+      <NavLink to={"/favourites"}></NavLink>
+      <NavLink to={"/messages"}></NavLink>
+      <NavLink to={"/notifications"}></NavLink>
+      <NavLink to={"/profile"}></NavLink>
+    </div>
+  );
 }
 
 export default Sidebar;
