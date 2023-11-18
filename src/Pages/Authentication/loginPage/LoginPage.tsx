5ea80437ac1d70f6bf9d1b6c5522cd3491d7b06a
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-11-18 16:18:52 +0400

    Update Login Page

--- src/Pages/Authentication/loginPage/LoginPage.tsx
+++ src/Pages/Authentication/loginPage/LoginPage.tsx
@@ -1,7 +1,65 @@
+import { useState } from "react";
 import logo from "/logo.svg";
 import { AiFillGoogleSquare } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userNameError, setUserNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  async function loginUser(userName: string, password: string) {
+    const data = {
+      username: userName,
+      password: password,
+    };
+    try {
+      const response = await fetch(
+        "https://instagram-api-88fv.onrender.com/api/register",
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        console.log("Success! Login completed.");
+        console.log(response.json());
+        setTimeout(() => {
+          navigate("/feed");
+        }, 3000);
+      } else {
+        console.error("Login failed with status:", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  function handleUserNameChange(value: string) {
+    setUserName(value);
+    if (value.length === 0) {
+      setUserNameError("Username is required");
+    } else {
+      setUserNameError("");
+    }
+  }
+
+  function handlePasswordChange(value: string) {
+    setPassword(value);
+    if (value.length === 0) {
+      setPasswordError("Password is required");
+    } else {
+      setPasswordError("");
+    }
+  }
+
   return (
     <div className="w-[100vw] h-[100vh]  flex items-center justify-center">
       <div className="w-[450px]">
@@ -31,12 +89,14 @@ function LoginPage() {
 
         <form action="" className=" flex flex-col justify-between p-10 gap-5">
           <input
-            type="email"
+            type="text"
             name=""
             id=""
-            placeholder="Email or Username"
+            placeholder="Username"
             className="px-4 py-2 bg-[#fafafa]"
+            onChange={(e) => handleUserNameChange(e.target.value)}
           />
+          {userNameError && <p className="text-red-500">{userNameError}</p>}
 
           <input
             type="password"
@@ -44,7 +104,9 @@ function LoginPage() {
             id=""
             placeholder="Password"
             className="px-4 py-2 bg-[#fafafa]"
+            onChange={(e) => handlePasswordChange(e.target.value)}
           />
+          {passwordError && <p className="text-red-500">{passwordError}</p>}
           <div className="flex flex-col gap-4 text-center  text-sm">
             <button>
               <p className="text-[#00376b]">Forgot password?</p>
@@ -53,8 +115,12 @@ function LoginPage() {
 
           <input
             type="submit"
-            value="Sign up"
+            value="Login"
             className="bg-[#0095f6] hover:bg-[#1877f2] cursor-pointer w-[80%] py-[6px] rounded-lg text-white font-bold self-center"
+            onClick={(e) => {
+              loginUser(userName, password);
+              e.preventDefault();
+            }}
           />
         </form>
       </div>
