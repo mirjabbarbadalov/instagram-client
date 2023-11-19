56ada81d16ea0c4f5c1672d301409a4644a914e0
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-19 21:36:12 +0400

    Add: Login & Register Page navlink test

--- src/pages/More/More.tsx
+++ src/pages/More/More.tsx
@@ -1,3 +1,17 @@
+import { NavLink } from "react-router-dom";
+import { RiLoginCircleFill } from "react-icons/ri";
+import { IoMdPersonAdd } from "react-icons/io";
 export default function More() {
-  return <div>More</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-10">
+      <NavLink to={"/login"}>
+        <RiLoginCircleFill className="text-[35px]" />
+        Login
+      </NavLink>
+      <NavLink to={"/register"}>
+        <IoMdPersonAdd className="text-[40px]" />
+        Register
+      </NavLink>
+    </div>
+  );
 }
