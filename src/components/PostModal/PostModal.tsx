ecd191316281282dce4ba5e1fa31254febb68c46
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-25 11:54:44 +0400

    refactor: fixed add post modal on home page

--- src/components/PostModal/PostModal.tsx
+++ src/components/PostModal/PostModal.tsx
@@ -72,7 +72,7 @@ function PostModal() {
     setIsModalOpen(!isModalOpen);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     setTitleError("");
     setImgUrlError("");
@@ -186,7 +186,7 @@ function PostModal() {
                 </div>
                 <button
                   type="submit"
-                  className="text-white inline-flex items-center bg-slate-800 bg-slate-950 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white inline-flex items-center bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   <svg
                     className="me-1 -ms-1 w-5 h-5"
