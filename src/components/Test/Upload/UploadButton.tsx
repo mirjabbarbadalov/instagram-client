a4ca063ff90422a5c40a83eb29f421da1be0d00f
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-09 16:04:35 +0400

    refactor: multiple changes

---
+++ src/components/Test/Upload/UploadButton.tsx
@@ -0,0 +1,7 @@
+export const UploadButton = () => {
+  return (
+    <div>
+      <button type="button">Upload</button>
+    </div>
+  );
+};
---
+++ src/components/Test/Upload/UploadModal.tsx
@@ -0,0 +1,7 @@
+export const UploadModal = () => {
+
+  
+  return <div>
+
+  </div>;
+};
