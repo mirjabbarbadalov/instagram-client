715f9b966710525ebbcec4fc2d728a399ce14b80
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-11 23:03:32 +0400

    add: initial messaging features

--- src/test/Chats/Message.tsx
+++ src/test/Chats/Message.tsx
@@ -2,6 +2,7 @@ export const Message = () => {
   return (
     <div>
       <img src="../../../public/images/messages.png" alt="" />
+      <p>Your messages</p>
       <p>Send private messages to a friend</p>
     </div>
   );
