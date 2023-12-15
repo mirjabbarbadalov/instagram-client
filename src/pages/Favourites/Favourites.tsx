fb8e9599c67d2974981d23424d268edc30ea716a
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-15 16:45:12 +0400

    add: updated favourite page logic and more page

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { CircularProgress } from "@mui/material";
+import { useEffect, useState, useCallback } from "react";
 import { useSelector } from "react-redux";
 import Post from "../../components/Post/Post";
 import { RootState } from "../../store/store";
@@ -6,10 +7,12 @@ import { PostData } from "../../types/types";
 
 function Favourites() {
   const [favoritePosts, setFavoritePosts] = useState<PostData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   const { user } = useSelector((state: RootState) => state.profile);
 
-  async function getFavoritePosts() {
+  const getFavoritePosts = useCallback(async () => {
     try {
       const data = await fetch(
         "https://instagram-api-88fv.onrender.com/api/posts"
@@ -23,34 +26,53 @@ function Favourites() {
       setFavoritePosts(userLikedPosts);
     } catch (error) {
       console.error("Error fetching posts:", error);
+      setError("Error fetching posts");
+    } finally {
+      setLoading(false);
     }
-  }
+  }, [user.id]);
 
   useEffect(() => {
     getFavoritePosts();
-  }, [favoritePosts]);
+  }, [getFavoritePosts]);
 
   console.log(user.id);
   console.log(favoritePosts);
 
   return (
-    <div className="flex flex-row gap-5 py-6 ml-[150px] w-[90%] flex-wrap">
-      {!favoritePosts.length && (
-        <p className="ml-[150px] text-lg">Your favorites are loading...</p>
-      )}
-      {favoritePosts.map((post) => (
-        <Post
-          key={post.title}
-          postData={post}
-          isFriend={false}
-          isProfile={false}
-          isFavorite={true}
-          index={0}
-          onClick={function (): void {
-            throw new Error("Function not implemented.");
-          }}
-        />
-      ))}
+    <div>
+      <h1 className="text-2xl mb-4 mt-10 ml-32">
+        Take a look at your favourites:
+      </h1>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row gap-5 py-6 ml-[150px] w-[90%] flex-wrap">
+          {loading && (
+            <div className="ml-48 mt-20">
+              <CircularProgress size={70} />
+            </div>
+          )}
+          {error && <p className="ml-[150px] text-lg text-red-500">{error}</p>}
+          {!loading && !error && favoritePosts.length === 0 && (
+            <p className="ml-[150px] text-lg">No favorite posts found.</p>
+          )}
+          {!loading &&
+            !error &&
+            favoritePosts.length > 0 &&
+            favoritePosts.map((post) => (
+              <Post
+                key={post.title}
+                postData={post}
+                isFriend={false}
+                isProfile={false}
+                isFavorite={true}
+                index={0}
+                onClick={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/More/More.tsx
+++ src/pages/More/More.tsx
@@ -1,41 +1,193 @@
-export default function More() {
+import React, { useState } from "react";
+
+const More: React.FC = () => {
+  const emailAddress = "aliasifzade@gmail.com";
+  const emailAddress2 = "mirjabbarbadalov@gmail.com";
+
+  const [isFrontendOpen, setIsFrontendOpen] = useState(true);
+  const [isBackendOpen, setIsBackendOpen] = useState(false);
+  const [isContactOpen, setIsContactOpen] = useState(false);
+
+  const toggleSection = (section: string) => {
+    setIsFrontendOpen(section === "frontend" ? !isFrontendOpen : false);
+    setIsBackendOpen(section === "backend" ? !isBackendOpen : false);
+    setIsContactOpen(section === "contact" ? !isContactOpen : false);
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center gap-10 p-10 mr-10">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente,
-        debitis. Numquam officiis odit eum commodi, blanditiis iure inventore
-        facilis architecto ullam vel consequuntur quod fugit porro ratione nisi
-        at reprehenderit molestias velit iste tempora veniam praesentium
-        perspiciatis asperiores doloremque. Repellat dolor, voluptatibus autem
-        assumenda ratione illo, in id, fuga magnam aperiam debitis amet atque
-        nesciunt vitae sed. Doloremque praesentium sit amet quis sunt vero
-        blanditiis nulla animi dicta iste. Expedita molestiae saepe repellat
-        laboriosam ipsum dolores commodi quis at possimus quaerat nihil facere
-        quod recusandae, temporibus assumenda sapiente minus quas aperiam.
-        Officia, consectetur iure. Porro recusandae sint eos cupiditate enim aut
-        consectetur cum maxime, pariatur dicta sunt quam, dolorum ad officia
-        aliquam eius harum culpa placeat similique nisi. Fugit, tempora ea
-        dolorum suscipit ratione odio nihil aut. Nihil esse saepe, suscipit at
-        eaque nesciunt! Quasi fugit necessitatibus repudiandae beatae? Veniam,
-        earum laudantium. Incidunt deleniti iste soluta facilis unde dignissimos
-        commodi architecto qui eum quidem natus esse, porro, expedita ipsam
-        aspernatur minus vitae sed non minima alias voluptate ut a quos fuga.
-        Quos error mollitia velit optio. Doloremque officiis neque qui veniam
-        laborum. Facere, molestias iusto? Repudiandae necessitatibus accusamus
-        facilis tenetur dignissimos accusantium magni ducimus minus quisquam?
-        Quasi eveniet libero voluptates tempore, vel maiores? Neque accusamus
-        quam deleniti molestiae expedita doloribus quia ipsum omnis, soluta
-        itaque vero assumenda consequuntur quidem excepturi incidunt explicabo
-        natus adipisci quis tenetur laborum numquam? Praesentium iure culpa,
-        vitae similique rem dignissimos quia sint, deleniti nostrum consectetur
-        fugiat, fugit consequuntur! Perferendis corporis, beatae, doloribus
-        nostrum quas quasi tempore incidunt iusto consequuntur, vitae architecto
-        natus quia nemo odio recusandae fuga dolorum iure delectus accusantium
-        molestias quam officia ut hic. Rem excepturi ratione, asperiores
-        explicabo veniam aliquam officia dolorem autem numquam deleniti iste.
-        Praesentium natus officiis ratione nostrum sit voluptatum suscipit animi
-        exercitationem rem veritatis aliquam, aliquid odit et.
-      </p>
+    <div className="ml-20 flex flex-col justify-center items-center w-[850px]">
+      {/* Frontend Box */}
+      <div
+        className={`border rounded p-5 shadow-lg m-4 w-[800px] flex-wrap ${
+          isFrontendOpen ? "bg-blue-100" : ""
+        }`}
+        onClick={() => toggleSection("frontend")}
+      >
+        <h2 className="text-2xl mb-3 font-bold cursor-pointer">
+          Client Technologies 🌐
+        </h2>
+        {isFrontendOpen && (
+          <div className="border rounded p-5 m-4 w-[800px] flex-wrap">
+            <ul>
+              <li>
+                <strong>React:</strong> Powering our sleek and dynamic user
+                interface.
+              </li>
+              <li>
+                <strong>Vite:</strong> Streamlining development with a speedy
+                build tool for optimized production builds.
+              </li>
+              <li>
+                <strong>TypeScript:</strong> Adding reliability and sanity to
+                our JavaScript.
+              </li>
+              <li>
+                <strong>Tailwind CSS:</strong> Styling made easy with our
+                utility-first framework.
+              </li>
+            </ul>
+            {/* State Management */}
+            <h2>State Management:</h2>
+            <ul>
+              <li>
+                <strong>Redux Toolkit:</strong> Simplifying state management in
+                our React app.
+              </li>
+              <li>
+                <strong>React Redux:</strong> Seamless connection of React
+                components to the Redux store.
+              </li>
+            </ul>
+            {/* Routing */}
+            <h2>Routing:</h2>
+            <ul>
+              <li>
+                <strong>React Router:</strong> Effortless navigation and routing
+                in our React applications.
+              </li>
+            </ul>
+            {/* Linting */}
+            <h2>Linting:</h2>
+            <ul>
+              <li>
+                <strong>ESLint:</strong> Maintaining a clean and error-free
+                codebase.
+              </li>
+            </ul>
+            {/* Build Tools */}
+            <h2>Build Tools:</h2>
+            <ul>
+              <li>
+                <strong>Vite:</strong> Empowering both development and
+                production builds for efficiency.
+              </li>
+            </ul>
+            {/* CSS Processing */}
+            <h2>CSS Processing:</h2>
+            <ul>
+              <li>
+                <strong>PostCSS:</strong> Effortless transformation of styles.
+              </li>
+            </ul>
+            {/* Other Dependencies */}
+            <h2>Other Dependencies:</h2>
+            <ul>
+              <li>
+                <strong>Axios:</strong> Handling HTTP requests seamlessly with
+                Axios.
+              </li>
+              <li>
+                <strong>Socket.IO Client:</strong> Enabling real-time
+                communication between clients and servers.
+              </li>
+            </ul>
+            {/* TypeScript Definitions */}
+          </div>
+        )}
+      </div>
+      {/* Backend Box */}
+      <div
+        className={`border rounded p-5 shadow-lg m-4 w-[800px] ${
+          isBackendOpen ? "bg-green-100" : ""
+        }`}
+        onClick={() => toggleSection("backend")}
+      >
+        <h2 className="text-2xl mb-3 font-bold cursor-pointer">
+          Server Technologies 🖥️
+        </h2>
+        {isBackendOpen && (
+          <div className="flex flex-col flex-wrap">
+            <div className=" w-[800px] flex flex-col flex-wrap p-8">
+              <ul>
+                <li>
+                  <strong>Node.js:</strong> Runtime for executing JavaScript
+                  code on the server-side.
+                </li>
+                <li>
+                  <strong>Express:</strong> Fast, unopinionated, minimalist web
+                  framework for Node.js.
+                </li>
+
+                <li>
+                  <strong>Mongodb:</strong> a NoSQL database that provides a
+                  flexible, schema-less data model and is widely used for its
+                  scalability and ease of development.
+                </li>
+
+                <li>
+                  <strong>Passport:</strong> a Node.js authentication middleware
+                  that simplifies the integration of various authentication
+                  strategies, such as username and password, social media
+                  logins, and third-party authentication providers, into web
+                  applications.
+                </li>
+                <li>
+                  <strong>JWT:</strong> library for creating, verifying, and
+                  decoding JSON Web Tokens (JWTs), commonly used for secure and
+                  compact representation of authentication and authorization
+                  claims in web development
+                </li>
+                <li>
+                  <strong>Bcrypt:</strong> password hashing library in Node.js
+                  that securely hashes and verifies passwords, providing a
+                  robust and adaptive cryptographic technique to safeguard user
+                  credentials in applications.
+                </li>
+                <li>
+                  <strong>Socket.io:</strong> JavaScript library that enables
+                  real-time, bidirectional communication between clients and
+                  servers, facilitating the creation of interactive and dynamic
+                  web applications with features like live updates, chat
+                  functionality, and collaborative editing.
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* Contact */}
+      <div
+        className={`border rounded p-5 shadow-lg m-4 w-[800px] ${
+          isContactOpen ? "bg-yellow-100" : ""
+        }`}
+        onClick={() => toggleSection("contact")}
+      >
+        <h2 className="text-2xl mb-3 font-bold cursor-pointer">
+          Contact Us 🧙‍♀️🧙‍♀️
+        </h2>
+        {isContactOpen && (
+          <p className="">
+            If you have any questions or want to discuss any aspect of our
+            project, feel free to reach out at:
+            <div className="flex flex-col gap-5 mt-5">
+              <a href={`mailto:${emailAddress}`}>{emailAddress}</a>
+              <a href={`mailto:${emailAddress2}`}>{emailAddress2}</a>
+            </div>
+          </p>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default More;
