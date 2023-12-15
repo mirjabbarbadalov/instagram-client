948bbf15f66196c3aee8d05abde8790a988d43c3
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-15 12:28:12 +0400

    fix: user filtering logic on suggestions

--- src/components/Suggestions/Suggestions.tsx
+++ src/components/Suggestions/Suggestions.tsx
@@ -8,49 +8,55 @@ import { fetchAllUsers } from "../../store/slices/allUserSlice";
 import { RootState } from "../../store/store";
 import { State } from "../../types/types";
 
+interface User {
+  id: string;
+  username: string;
+  profilePhoto: string | null;
+}
+
 export default function Suggestions() {
   const dispatch = useDispatch<ThunkDispatch<State, void, Action>>();
   const { user } = useSelector((state: RootState) => state.profile);
-  const userId = user?.id;
 
   useEffect(() => {
     dispatch(fetchProfileDetails());
   }, [dispatch]);
 
   useEffect(() => {
-    if (userId) {
+    if (user?.id) {
       dispatch(fetchAllUsers());
     }
-  }, [userId, dispatch]);
-
-  interface User {
-    id: string;
-    username: string;
-    profilePhoto: string | null;
-  }
+  }, [user?.id, dispatch]);
 
   const allUsers: User[] = useSelector((state: RootState) => state.user.users);
+  console.log("User:", user);
+  console.log("All Users:", allUsers);
+  const suggestedUsers = allUsers.filter(
+    (suggestedUser) => suggestedUser.username !== user?.username
+  );
+
+  console.log("Suggested Users:", suggestedUsers);
 
   return (
     <div className="z-0">
       <p className="ml-[6px] mb-3 text-sm">Suggested for you:</p>
       <div className="w-[300px] max-h-[200px] overflow-auto p-3 border-l flex flex-col gap-1">
-        {allUsers && allUsers.length > 0 ? (
-          allUsers.map((user, index) => (
-            <NavLink key={index} to={`/friend/${user.username}`}>
+        {suggestedUsers && suggestedUsers.length > 0 ? (
+          suggestedUsers.map((suggestedUser, index) => (
+            <NavLink key={index} to={`/friend/${suggestedUser.username}`}>
               <div className="flex items-center gap-2 cursor-pointer w-[100%]">
                 <div className="">
-                  {user.profilePhoto ? (
+                  {suggestedUser.profilePhoto ? (
                     <img
                       className="rounded-full w-[40px] h-[40px] object-cover"
-                      src={`data:image/jpeg;base64,${user.profilePhoto}`}
+                      src={`data:image/jpeg;base64,${suggestedUser.profilePhoto}`}
                       alt="profile photo"
                     />
                   ) : (
                     <div className="rounded-full w-[40px] h-[40px] object-cover bg-slate-200"></div>
                   )}
                 </div>
-                <p>{user.username}</p>
+                <p>{suggestedUser.username}</p>
               </div>
             </NavLink>
           ))
